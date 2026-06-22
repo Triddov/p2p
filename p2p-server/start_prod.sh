419fd337e6a3ba92087d6bab5d09ac62eb4cdf7c
Triddov <akinin.stepan7@gmail.com>
2026-06-22 18:35:03 +0300

    ci: make jobs parallel, add cache for gradle, add explicit prod start .sh

---
+++ p2p-server/start_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+docker-compose pull
+docker-compose up -d 
+docker image prune -f
+
+sleep 5
+if docker-compose ps --format '{{.Name}} {{.State}}' | grep -vw 'running'; then
+  echo "=== Services are not running: ==="
+  docker compose logs --tail=50
+  exit 1
+fi
